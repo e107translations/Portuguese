d1c233b454b122f6ebe51e4e891390142c058d6a
rica-carv <rica-carv@users.noreply.github.com>
2018-09-05 19:24:59 +0100

    Add files via upload

--- e107_languages/Portuguese/Portuguese.php
+++ e107_languages/Portuguese/Portuguese.php
@@ -4,7 +4,7 @@
 |        e107 website content management system Portuguese Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/08/19 19:00:15
+|        Last Modified: 2018/09/05 18:24:37
 |
 |        $Author: Ricardo $
 +---------------------------------------------------------------+
@@ -13,18 +13,18 @@
 setlocale(LC_ALL,  'pt_PT.UTF-8', 'pt_PT.utf8',  'pt');
 define("CORE_LC", "pt");
 define("CORE_LC2", "pt");
-define("CORE_LAN1", "Erro: o tema não existe.\\n\\nMude os temas usados em preferências (área administrativa) ou envie os arquivos para o tema atual para o servidor.");
-define("CORE_LAN4", "Por favor, apague o install.php do seu servidor");
-define("CORE_LAN5", "Se você não fizer isso, há um potencial risco de segurança para o seu site");
-define("CORE_LAN6", "A proteção contra as inundações neste site foi ativada e você está avisado de que se continuar solicitando páginas você pode ser banido.");
-define("CORE_LAN7", "Núcleo está a tentar restaurar as preferências a partir da cópia de segurança automática.");
+define("CORE_LAN1", "Erro: o tema não existe.\\n\\nMude os temas usados em suas preferências (área administrativa) ou faça o envio de arquivos referentes ao tema atual para o servidor.");
+define("CORE_LAN4", "Por favor, apague o ficheiro install.php do seu servidor");
+define("CORE_LAN5", "Se não fizer isso, há um potencial risco de segurança para o seu site");
+define("CORE_LAN6", "A proteção contra abusos neste site foi ativada e está avisado de que, se continuar solicitando páginas você pode ser banido.");
+define("CORE_LAN7", "O núcleo está tentando restaurar as preferências de uma cópia de segurança automática.");
 define("CORE_LAN8", "Erro nas preferências do núcleo");
-define("CORE_LAN9", "Núcleo não conseguiu restaurar a partir da cópia de segurança automática. Execução interrompida.");
-define("CORE_LAN10", "Cookie corrompido detectado - desligado.");
+define("CORE_LAN9", "Núcleo não conseguiu restaurar da cópia de segurança automática. Execução interrompida.");
+define("CORE_LAN10", "Biscoito corrompido detectado - desligado.");
 define("CORE_LAN11", "Tempo de renderização:");
 define("CORE_LAN12", "seg (");
-define("CORE_LAN13", "% do que para consultas)");
-define("CORE_LAN14", "%2.3f s de cpu (carga de %2.2f%%, %2.3f inicialização). Relógio:");
+define("CORE_LAN13", "% para consultas)");
+define("CORE_LAN14", "%2.3F s de cpu (carga de %2.2f%%, %2.3f inicialização). Relógio:");
 define("CORE_LAN15", "Consultas de DB:");
 define("CORE_LAN16", "Memória:");
 define("CORE_LAN17", "[imagem desabilitada]");
@@ -39,26 +39,26 @@ define("EMESSLAN_TITLE_ERROR", "Erro");
 define("EMESSLAN_TITLE_SUCCESS", "Sucesso");
 define("EMESSLAN_TITLE_WARNING", "Aviso");
 define("EMESSLAN_TITLE_DEBUG", "Sistema de depuração");
-define("LAN_NO_PERMISSIONS", "Não tem permissões para visualizar esta página.");
+define("LAN_NO_PERMISSIONS", "Não tem permissão para ver esta página.");
 define("LAN_EDIT", "Editar");
 define("LAN_DELETE", "Excluir");
 define("LAN_DEFAULT", "Padrão");
-define("LAN_MORE", "Mais...");
+define("LAN_MORE", "Mais..");
 define("LAN_LESS", "..Menos");
-define("LAN_READ_MORE", "Leia mais...");
-define("LAN_GOPAGE", "Ir para a página");
-define("LAN_GOTOPAGEX", "Ir para a página [x]");
+define("LAN_READ_MORE", "Leia mais..");
+define("LAN_GOPAGE", "Ir para página");
+define("LAN_GOTOPAGEX", "Ir para página [x]");
 define("LAN_GO", "Ir");
 define("LAN_SUBMIT", "Enviar");
 define("LAN_NONE", "Nenhum");
 define("LAN_WARNING", "Aviso!");
 define("LAN_ERROR", "Erro");
 define("LAN_ANONYMOUS", "Anônimo");
-define("LAN_EMAIL_SUBS", "-e-mail-");
+define("LAN_EMAIL_SUBS", "-correio electrónico-");
 define("LAN_ACTIVE", "Activo");
 define("LAN_YES", "Sim");
 define("LAN_NO", "Não");
-define("LAN_OK", "OK");
+define("LAN_OK", "Razoável");
 define("LAN_ACTIONS", "Ações");
 define("LAN_THANK_YOU", "Obrigado");
 define("LAN_CONTINUE", "Continue");
@@ -86,25 +86,26 @@ define("LAN_IP_ADDRESS", "Endereço IP");
 define("LAN_AUTHOR", "Autor");
 define("LAN_CATEGORY", "Categoria");
 define("LAN_GUEST", "Convidado");
-define("LAN_NEXT", "Próxima");
+define("LAN_NEXT", "Próximo");
 define("LAN_PREVIOUS", "Anterior");
-define("LAN_LOGIN", "Login");
-define("LAN_LOGOUT", "Logout");
+define("LAN_LOGIN", "Entrar");
+define("LAN_LOGOUT", "Sair");
 define("LAN_SETTINGS", "Configurações");
 define("LAN_PASSWORD", "Senha");
 define("LAN_INCORRECT_PASSWORD", "Senha incorrecta");
 define("LAN_TYPE", "Tipo");
 define("LAN_SCREENSHOT", "Captura de tela");
 define("LAN_FILE", "Arquivo");
-define("LAN_YOUTUBE_VIDEO", "Video Youtube");
+define("LAN_YOUTUBE_VIDEO", "Video do Youtube");
 define("LAN_YOUTUBE_PLAYLIST", "Lista de reprodução do Youtube");
 define("LAN_FILETYPES", "Tipos de arquivos");
 define("LAN_FILE_NOT_FOUND", "Arquivo não encontrado");
 define("LAN_FILES", "Arquivos");
 define("LAN_SIZE", "Tamanho");
 define("LAN_VERSION", "Versão");
-define("LAN_DOWNLOAD", "Baixar");
-define("LAN_WEBSITE", "Website");
+define("LAN_DOWNLOAD", "Descarregar");
+define("LAN_DOWNLOAD_NO_PERMISSION", "Ficheiro não encontrado ou não tem permissões para descarregar este ficheiro");
+define("LAN_WEBSITE", "Sítio web");
 define("LAN_COMMENTS", "Comentários");
 define("LAN_LOCATION", "Localização");
 define("LAN_NO_RECORDS_FOUND", "Nenhum registro encontrado");
@@ -113,21 +114,19 @@ define("LAN_IMAGE", "Imagem");
 define("LAN_ABOUT", "Sobre");
 define("LAN_TITLE", "Título");
 define("LAN_MESSAGE", "Mensagem");
-define("LAN_USER", "Usuário");
-define("LAN_EMAIL", "Endereço de e-mail");
+define("LAN_USER", "Utilizador");
+define("LAN_EMAIL", "Endereço de correio electrónico");
 define("LAN_WROTE", "escreveu");
 define("LAN_RE_ORDER", "Re-ordenar");
-define("LAN_RELATED", "Relacionado");
+define("LAN_RELATED", "Relativo");
 define("LAN_CLOSE", "Fechar");
 define("LAN_EXPAND", "Expandir");
-define("LAN_ENTER_USRNAME_EMAIL", "Por favor, digite seu nome de usuário ou e-mail");
-define("LAN_PWD_REQUIRED", "Senha é necessária");
-define("LAN_SHOW", "Mostrar");
+define("LAN_LIST", "Lista");
+define("LAN_ENTER_USRNAME_EMAIL", "Por favor, digite o seu nome de utilizador ou correio electrónico");
+define("LAN_PWD_REQUIRED", "É necessária uma senha");
+define("LAN_SHOW", "Exibir");
 define("LAN_GENERATE", "Gerar");
 define("LAN_SUMMARY", "Sumário");
-define("LAN_REQUIRED_BLANK", "Campo(s) necessário(s) estão em branco.");
+define("LAN_REQUIRED_BLANK", "Campo(s) necessário(s) deixado(s) em branco");
 define("LAN_PLEASEWAIT", "Por favor espere");
 define("LAN_CHOOSE_FILE", "Escolha um ficheiro");
-
-
-?>
