cedcd337cd5b31d93272b112ad9c6beafd094fc2
rica-carv <rica-carv@users.noreply.github.com>
2018-09-05 19:08:12 +0100

    Add files via upload

--- e107_languages/Portuguese/admin/lan_lancheck.php
+++ e107_languages/Portuguese/admin/lan_lancheck.php
@@ -1,36 +1,45 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Portuguese Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2018/09/05 18:05:01
+|
+|        $Author: Ricardo $
++---------------------------------------------------------------+
+*/
 
 define("LAN_CHECK_2", "Verifique se");
 define("LAN_CHECK_3", "Verificação de");
 define("LAN_CHECK_4", "Arquivo faltando!");
 define("LAN_CHECK_5", "Frase faltando!");
-define("LAN_CHECK_15", "Os caracteres ilegais ou espaços encontrados antes [x] ou [y]");
+define("LAN_CHECK_15", "Os caracteres ilegais ou espaços encontrados antes de [x] ou depois de [y]");
 define("LAN_CHECK_16", "Arquivo original");
 define("LAN_CHECK_17", "Um problema de gravação ocorreu ao tentar salvar o arquivo.");
 define("LAN_CHECK_18", "Arquivos no formato padrão de idioma não estão disponíveis para este plugin/tema.");
 define("LAN_CHECK_19", "Non-UTF-8 caracteres encontrados!");
 define("LAN_CHECK_20", "Gerar o pacote de idioma");
 define("LAN_CHECK_21", "Verifique novamente");
-define("LAN_CHECK_22", "Tema");
 define("LAN_CHECK_23", "Erros encontrados");
-define("LAN_CHECK_24", "Resumo");
-define("LAN_CHECK_25", "Temas");
+define("LAN_CHECK_26", "Frente");
 define("LAN_CHECK_PAGE_TITLE", "Idiomas");
 define("LAN_CHECK_27", "Número de erros de pacote de idiomas encontrados");
+define("LAN_CHECK_28", "Indêntico");
+define("LAN_CHECK_29", "Texto idêntico (apenas aviso)");
+define("LAN_CHECK_30", "Faltam bbcodes");
+define("LAN_CHECK_31", "Faltam os caracter(es) [ e/ou ]");
+define("LAN_CHECK_32", "Falta síntaxe HTML");
 define("LANG_LAN_23", "Criar o pacote de idiomas (zip)");
 define("LANG_LAN_30", "Data de lançamento");
 define("LANG_LAN_31", "Compatibilidade");
-define("LANG_LAN_35", "Os pacotes de idiomas estão disponíveis para esta versão do e107.");
+define("LANG_LAN_35", "Os seguintes pacotes de idiomas estão disponíveis para esta versão do e107.");
 define("LANG_LAN_111", "Data de lançamento");
 define("LANG_LAN_112", "Compatível");
 define("LANG_LAN_114", "Baixar pacote");
 define("LANG_LAN_115", "Por favor verificar e corrigir os restantes [x] erro (s) antes de tentar criar um pacote de idiomas.");
-define("LANG_LAN_116", "Por favor verifique se seus arquivos de idioma ('verificar'), em seguida, tente novamente.");
+define("LANG_LAN_116", "Por favor verifique os seus arquivos de idioma ('verificar'), e em seguida, tente novamente.");
 define("LANG_LAN_117", "Você deve corrigir os erros restantes antes de contribuir com seu pacote de idiomas.");
-define("LANG_LAN_119", "Por favor verifique se CORE_LC e CORE_LC2 têm valores em [x] e tentar novamente.");
-define("LANG_LAN_120", "Por favor, certifique-se você estiver usando nomes de pasta padrão em e107_config.php (eg. e107_languages /, e107_plugins / etc) e tente novamente.");
-define("LANG_LAN_AGR", "Nota: Usando essas ferramentas você concorda compartilhar seu pacote de idioma (s) com a comunidade de e107.");
+define("LANG_LAN_119", "Por favor verifique se CORE_LC e CORE_LC2 têm valores em [x] e tente novamente.");
+define("LANG_LAN_120", "Por favor, certifique-se que está a usar nomes de pasta padrão em e107_config.php (eg. e107_languages /, e107_plugins / etc) e tente novamente.");
+define("LANG_LAN_AGR", "Nota: Usando essas ferramentas você concorda compartilhar seu pacote de idioma(s) com a comunidade e107.");
